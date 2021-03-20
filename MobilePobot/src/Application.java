f2f650f793431cb32255e81f5a4fe5e73f6a0ec0
rado-stoyanov <rado.stoyanow@gmail.com>
2021-03-20 12:21:38 +0200

    first version

---
+++ MobilePobot/src/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		int serialNumber 		= Console.promt("Please enter serial number: ");
+		MenuProcessor.orderMenuItem(serialNumber);
+
+	}
+}
