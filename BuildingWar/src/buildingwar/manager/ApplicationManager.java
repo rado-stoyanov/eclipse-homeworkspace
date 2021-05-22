de8930872a761e374587fff0a996bc702d12c92c
rado-stoyanov <rado.stoyanow@gmail.com>
2021-05-22 19:22:15 +0300

    Clear warnings

--- BuildingWar/src/buildingwar/manager/ApplicationManager.java
+++ BuildingWar/src/buildingwar/manager/ApplicationManager.java
@@ -29,16 +29,11 @@ public class ApplicationManager {
 	
 	
 	private void loadGamePlay() {
-		
-//		String actionKey = Console.promtString("Choose squad direction: ");
-//		if(KeyManager.isDirectionKey(actionKey)) {
-//			new SquadManager().processActionMove(actionKey);
-//		}
-		////////////////////////////////////////////////////////////////////
-		Console.logln("Gameplay options:");
+
+		Console.logln("Action options:");
 		Console.logln("1. Move");
-		Console.logln("2. Change active main Soldier");
-		int modeKey = Console.promtInt("Choose mode options: ");
+		Console.logln("2. Change active main soldier");
+		int modeKey = Console.promtInt("Choose action option: ");
 
 		
 		if(modeKey == GamePlayStatus.MOVE_OPTION) {
@@ -48,14 +43,13 @@ public class ApplicationManager {
 			}
 		}
 		if(modeKey == GamePlayStatus.CHANGE_MAIN_SOLDIER) {
-			Console.logln("Change options: 1(Tank), 2(Sniper), 3(Spy), 4(Wrecker)");
-			int choosenSoldier = Console.promtInt("Choose soldier: ");
+			Console.logln("Squad options: 1(Tank), 2(Sniper), 3(Spy), 4(Wrecker)");
+			int choosenSoldier = Console.promtInt("Choose new main active soldier: ");
 			if(KeyManager.isSoldierKeyOption(choosenSoldier)) {
 				new SquadManager().changeMainSoldierPosition(choosenSoldier);
 			}
 		}
-				
-		////////////////////////////////////////////////////////////////////		
+		
 		Console.logln("");
 		Console.logln("");
 		this.render();
--- BuildingWar/src/buildingwar/manager/KeyManager.java
+++ BuildingWar/src/buildingwar/manager/KeyManager.java
@@ -10,10 +10,7 @@ public class KeyManager {
 	public static final int TANK 			= 1;
 	public static final int SNIPER 			= 2;
 	public static final int SPY 			= 3;
-	public static final int WRECKER 		= 4;
-	
-	//public static final String FIRE 		= "f";
-	
+	public static final int WRECKER 		= 4;	
 	
 	public static boolean isDirectionForward(String key) {
 		return key.equals(FORWARD);
@@ -47,11 +44,7 @@ public class KeyManager {
 				keyCodeParameter == SPY 	||
 				keyCodeParameter == WRECKER;
 	}
-	
-//	public static boolean isFireKey(String keyCodeParameter) {
-//		
-//		return 	process(keyCodeParameter).equals(FIRE);
-//	}
+
 	
 	private static String process(String keyCodeParameter) {
 		return keyCodeParameter.toLowerCase(); 
