6ef66dd6d3e4dea8f2c1fe315a3ec3b7c9d5aba3
rado-stoyanov <rado.stoyanow@gmail.com>
2021-08-08 17:20:18 +0300

    commet

--- Monopoli/src/Application.java
+++ Monopoli/src/Application.java
@@ -1,11 +1,15 @@
 public class Application {
 
 	public static void main(String[] args) {
-		
-		PlayerManager.createNewPlayer();	
-		GamePlotManager.addElementsToGamePlot();	
-		StepManager.move();
 
-		
+		String[] myArray = {"FALSESUCCESS", "TRUE", "MY", "SUCCESS", "BULGARIA", "SUCCESS"};
+
+		for(int i = 0; i < myArray.length ; i++){
+			
+			if(myArray[i] == "SUCCESS") {
+				System.out.println(i);
+			}
+
+		}	
 	}
 }
